128fd14837ce072ba6df8464183e138b2f35df05
Desu <673464228@qq.com>
2023-10-12 00:04:47 +0800

    feat: add user verify

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+
+import { Request, Response } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    /**
+     * 验证token值并返回基础信息
+     * @param req 
+     */
+    @Get('/verify')
+    verifyToken (@Req() req: Request, @Res() res: Response) {
+      const userToken = req.get('token')
+      if (!userToken) return res.json({
+        code: 500,
+        data: null,
+        msg: '无效token'
+      })
+
+      return res.json({
+        code: 200,
+        data: null,
+        msg: '有token'
+      })
+    }
+}
